MathCev/device_xiaomi_cepheus

cepheus: Enable and configure FOD HAL

device.mk
@@ -71,6 +71,10 @@ PRODUCT_PACKAGES += \
     libqdMetaData.system \
     libvulkan
 
+# Fingerprint
+PRODUCT_PACKAGES += \
+    lineage.biometrics.fingerprint.inscreen@1.0-service.xiaomi_cepheus
+
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0_system \

igit-cn/android_device_xiaomi_cepheus

Change-Id: I023e24a491818b9c9d9638e1a55cf9dbad9f684c

device.mk
@@ -71,6 +71,10 @@ PRODUCT_PACKAGES += \
     libqdMetaData.system \
     libvulkan
 
+# Fingerprint
+PRODUCT_PACKAGES += \
+    mokee.biometrics.fingerprint.inscreen@1.0-service.xiaomi_cepheus
+
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0_system \